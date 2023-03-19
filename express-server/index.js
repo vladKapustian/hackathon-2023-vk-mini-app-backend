118fc3381a3ce3e2ce47aada2710811bd0e5049a
unknown <od1nsky228@gmail.com>
2023-03-19 04:53:58 +0300

    Added api endpoints

--- express-server/index.js
+++ express-server/index.js
@@ -1,71 +1,73 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const sequelize = require('./sequelize')
+const sequelize = require("./sequelize");
 
-const Group = require('./models/Group/Group')
-const Lesson = require('./models/Lesson/Lesson')
-const Day = require('./models/Day/Day')
-const Schedule = require('./models/Schedule/Schedule')
+const Group = require("./models/Group/Group");
+const Lesson = require("./models/Lesson/Lesson");
+const Day = require("./models/Day/Day");
+const Schedule = require("./models/Schedule/Schedule");
 
-import {
-    getFaculties,
-    getCourses,
-    getGroups
-} from "./models/Group/groupController"
-import getSchedule from "./models/Schedule/scheduleController"
-
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello WWW!' });
-});
+import { getFaculties, getCourses, getGroups } from "./models/Group/groupController";
+import { getSchedule } from "./models/Schedule/scheduleController";
 
 const _startServer = async () => {
-    try {
-        await Promise.all([
-            sequelize.sync({ alter: true }),
-            sequelize.authenticate(),
-        ]).then(() => console.log("Connection has been established successfully"));
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
-}
+  try {
+    await Promise.all([sequelize.sync({ alter: true }), sequelize.authenticate()]).then(() =>
+      console.log("Connection has been established successfully")
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 _startServer();
 
 try {
-    Lesson.bulkCreate([
-        { 
-          subject: "Инфокоммуникационные системы, сети и телекоммуникации",
-          time: "10:10 — 11:40",
-          classroom: "ауд. 307",
-          teacher: "доц.Щербаков С.М."
-        },
-        { 
-          subject: "Математика",
-          time: "11:50 — 13:20",
-          classroom: "ауд. 605",
-          teacher: "доц.Лукьянова Г.В."
-        },
-    ]);
-    Day.bulkCreate([{ dayNumber: 1, LessonId: 2 },{ dayNumber: 2, LessonId: 1 }]);
-    Schedule.bulkCreate([{ DayId: 2 }, { DayId: 1 }]);
-    Group.bulkCreate([
-        {
-          faculty: "КТиБ",
-          course: 1,
-          group_name: "ИСТ-311",
-          ScheduleId: 1
-        },
-        {
-          faculty: "КТиБ",
-          course: 1,
-          group_name: "ПМИ-311",
-          ScheduleId: 2
-        },
-    ])
-  } catch (error) {
-    console.log("ERROR: ", error)
+  Lesson.bulkCreate([
+    {
+      subject: "Инфокоммуникационные системы, сети и телекоммуникации",
+      time: "10:10 — 11:40",
+      classroom: "ауд. 307",
+      teacher: "доц.Щербаков С.М.",
+    },
+    {
+      subject: "Математика",
+      time: "11:50 — 13:20",
+      classroom: "ауд. 605",
+      teacher: "доц.Лукьянова Г.В.",
+    },
+  ]);
+  Day.bulkCreate([
+    { dayNumber: 1, LessonId: 2 },
+    { dayNumber: 2, LessonId: 1 },
+  ]);
+  Schedule.bulkCreate([{ DayId: 2 }, { DayId: 1 }]);
+  Group.bulkCreate([
+    {
+      faculty: "КТиБ",
+      course: 1,
+      group_name: "ИСТ-311",
+      ScheduleId: 1,
+    },
+    {
+      faculty: "КТиБ",
+      course: 1,
+      group_name: "ПМИ-311",
+      ScheduleId: 2,
+    },
+  ]);
+} catch (error) {
+  console.log("ERROR: ", error);
 }
 
+app.get("/faculties", getFaculties);
+
+app.get("/coures", getCourses);
+
+app.get("/groups", getGroups);
+
+app.get("/schedule", getSchedule);
+
 app.listen(3333, () => {
-    console.log('Application listening on port 3333!');
+  console.log("Application listening on port 3333!");
 });
--- express-server/models/Schedule/scheduleController.js
+++ express-server/models/Schedule/scheduleController.js
@@ -1,34 +1,34 @@
-const Schedule = require("./Schedule")
-const Day = require("../Schedule/Schedule")
-const Lesson = require("../Lesson/Lesson")
+const Schedule = require("./Schedule");
+const Day = require("../Schedule/Schedule");
+const Lesson = require("../Lesson/Lesson");
 
 export const getSchedule = async (req, res) => {
-    try {
-        const schedule = await Schedule.findOne({
-            where: {
-                'id': req.body
-            }
-        });
-        
-        if(req.body.returnWeek) {
-            const day = await Day.findAll();
-        } else {
-            const day = await Day.findAll({
-            where: {
-                'id': schedule.DayId,
-                'dayNumber': req.body.dayNumber
-            }
-        });
-        }
-        
-        const lessons = await Lesson.findAll({
-            where: {
-                'id': day.LessonId
-            }
-        })
+  try {
+    const schedule = await Schedule.findOne({
+      where: {
+        id: req.body,
+      },
+    });
 
-        res.status(200).send(lessons);
-    } catch (error) {
-        res.status(500).send(error);
+    if (req.body.returnWeek) {
+      const day = await Day.findAll();
+    } else {
+      const day = await Day.findAll({
+        where: {
+          id: schedule.DayId,
+          dayNumber: req.body.dayNumber,
+        },
+      });
     }
-}
+
+    const lessons = await Lesson.findAll({
+      where: {
+        id: day.LessonId,
+      },
+    });
+
+    res.status(200).send(lessons);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
